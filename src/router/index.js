4ca3dff4ec6dee8737296bd553fc7a23f98ae6d9
lipeng0808 <997627132@qq.com>
2020-11-26 15:14:33 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,68 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+// 导入页面
+import Login from '../views/login/Login.vue'
+import Layout from '../views/layout/Index.vue'
+import Welcome from '../views/layout/welcome/Index.vue'
+import Chart from '../views/layout/chart/Index.vue'
+import User from '../views/layout/user/Index.vue'
+import Question from '../views/layout/question/Index.vue'
+import EnterPrice from '../views/layout/enterPrise/Index.vue'
+import Subject from '../views/layout/subject/Index.vue'
+import NotFound from '../views/404/NotFound.vue'
+
+
+const routes = [{
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    redirect: 'Login'
+  },
+  {
+    path: '/layout',
+    component: Layout,
+    children: [{
+        path: 'welcome',
+        component: Welcome
+      },
+      {
+        path: '',
+        component: Welcome
+      },
+      {
+        path: 'chart',
+        component: Chart
+      },
+      {
+        path: 'user',
+        component: User
+      },
+      {
+        path: 'question',
+        component: Question
+      },
+      {
+        path: 'enterPrice',
+        component: EnterPrice
+      },
+      {
+        path: 'subject',
+        component: Subject
+      },
+      {
+        path: '*',
+        component: NotFound
+      },
+    ]
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ]
 
---
+++ src/views/404/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="notfound">
+    <h1>NotFound 页面不存在哦</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
